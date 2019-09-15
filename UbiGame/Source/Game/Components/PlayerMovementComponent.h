b1472ece7b90c24bb5cabb8f8dcb567d39684c99
kiminno <nancyngo141@gmail.com>
2019-09-15 00:23:39 -0400

    Added dance

--- UbiGame/Source/Game/Components/PlayerMovementComponent.h
+++ UbiGame/Source/Game/Components/PlayerMovementComponent.h
@@ -12,13 +12,16 @@ namespace Game
 		virtual void Update() override;
 		virtual void OnAddToWorld() override;
 
+		void CheckBedVisit();
+
 	private:
-		int m_lastFaceIndex;
 		bool m_wasFaceSwapButtonPressed;
 		bool m_wasRightPressed;
 		bool m_wasLeftPressed;
 		bool m_wasUpPressed;
 		bool m_wasDownPressed;
 		int m_numSteps;
+		float m_x;
+		float m_y;
 	};
 }
